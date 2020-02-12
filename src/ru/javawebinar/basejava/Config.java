1e7a73e8bee76b8bf4ca69ea6ba8049b2461a5a0
Solomon3038 <anhalt79@gmail.com>
2020-02-12 18:32:50 +0100

    Mi First message

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -11,8 +11,8 @@ import java.util.Properties;
 
 public class Config {
 
-//   private static final String PROPS = "/resumes.properties";
- private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
+  // private static final String PROPS = "/resumes.properties";
+    private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
 
     private static final Config INSTANCE = new Config();
 
@@ -25,29 +25,27 @@ public class Config {
 
     private Config() {
 
-//        -------------------------------------------------------------------------------------
 
-//        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
-//            Properties props = new Properties();
-//            props.load(is);
-//            storageDir = new File(props.getProperty("storage.dir"));
-//            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
-//        } catch (IOException e) {
-//            throw new IllegalStateException("Invalid config file " + PROPS);
-//        }
-//    }
-//
-//    public File getStorageDir() {
-//        return storageDir;
-//    }
-//
-//    public Storage getStorage() {
-//        return storage;
-//    }
 
-    //        ---------------------------------------------------------------------------------------
+      /* try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
+           Properties props = new Properties();
+           props.load(is);
+           storageDir = new File(props.getProperty("storage.dir"));
+           storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
+        } catch (IOException e) {
+          throw new IllegalStateException("Invalid config file " + PROPS);
+      }
+    }
 
-//        ---------------------------------------------------------------------------------------
+   public File getStorageDir() {
+       return storageDir;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }*/
+
+
 
         try (InputStream is = new FileInputStream(PROPS)) {
                 Properties props = new Properties();
